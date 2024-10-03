8a14fc11295ab5cdde9ca292f37d690e2ad64fc3
Honorio <honorio.apzqgmail.com>
2024-10-02 22:32:39 -0500

    cuarto commit

--- src/components/CreativeCarousel.jsx
+++ src/components/CreativeCarousel.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { ChevronLeft, ChevronRight, Facebook, Linkedin } from 'lucide-react';
 
 const images = [
-  "fotos/foto10.png",
+  "fotos/foto10.jpg",
   "fotos/foto16.png",
   "fotos/foto9.jpg",
   "fotos/foto8.jpg",
