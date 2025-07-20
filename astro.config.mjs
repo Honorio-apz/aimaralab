e3c7677b0ad1515d8a235f006e9b8dbb7d18f22a
Honorio <honorio.apz@gmail.com>
2025-07-20 18:48:58 -0500

    actualizacion2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   integrations: [tailwind(),  sitemap(), react()],
   output: 'server',
+  adapter: vercel(),
 });
--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer';
-
+import 'dotenv/config';
 export const prerender = false;
 
 export async function POST({ request }) {
