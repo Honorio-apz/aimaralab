5ebbe059a9280466e636689a181155898fe0dc0c
Honorio <honorio.apz@gmail.com>
2025-07-20 17:38:48 -0500

    actualizacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from '@astrojs/react';
-import.meta.env;
 export default defineConfig({
-  site: 'https://lexingtonthemes.com',
   integrations: [tailwind(),  sitemap(), react()],
   output: 'server',
 });
