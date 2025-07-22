8868de0bbe5b4731beb864f4896e5f7a0e2249cd
Honorio <honorio.apz@gmail.com>
2025-07-21 22:42:01 -0500

    first commit blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+//import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 export default defineConfig({
   integrations: [tailwind(),  sitemap(), react()],
   output: 'server',
