ac6423f63b9c3df9cb06493ac6e8341e02a322cc
Honorio <honorio.apz@gmail.com>
2026-01-12 22:36:04 -0500

    la nueva version

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import react from '@astrojs/react';
 //import vercel from '@astrojs/vercel/serverless';
 import vercel from '@astrojs/vercel';
 export default defineConfig({
-  integrations: [tailwind(),  sitemap(), react()],
+  integrations: [tailwind(), sitemap(), react()],
   output: 'server',
   adapter: vercel(),
 });
+
+
